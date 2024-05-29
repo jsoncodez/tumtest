25e9e2b7e98828fdc921029fdc792a1de7cde419
jsoncodez <jason20@umbc.edu>
2024-05-29 02:39:41 -0400

    added print function to main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 
+using namespace std;
+
 int main() {
     std::cout << "gittest message" << std::endl;
+
+
+    cout << "added using namespace std" << endl;
     return 0;
 }
